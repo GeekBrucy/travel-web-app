ebd47a4c5d296b5f10419efff17358854257de7d
geekbrucy <peichunzhou1006@gmail.com>
2018-05-05 15:25:23 +1000

    project init

--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// for the click delay on mobile device
+import fastClick from 'fastClick'
+
+// import css
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+
 Vue.config.productionTip = false
+
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
