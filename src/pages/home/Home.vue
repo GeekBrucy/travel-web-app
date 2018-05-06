f3ae675e00a66c95da8b3537a75964da6bf06d06
geekbrucy <peichunzhou1006@gmail.com>
2018-05-06 13:06:15 +1000

    add icons with swiper"

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <app-header></app-header>
     <app-swiper></app-swiper>
+    <app-icons></app-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     appHeader: HomeHeader,
-    appSwiper: HomeSwiper
+    appSwiper: HomeSwiper,
+    appIcons: HomeIcons
   }
 }
 </script>
