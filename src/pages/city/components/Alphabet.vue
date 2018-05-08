b97a707ea5922ad6f779243973925400caaffe48
geekbrucy <peichunzhou1006@gmail.com>
2018-05-08 12:17:58 +1000

    imporve performance

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="list">
     <ul>
-      <li class="item" v-for="(item, key) of cities" :key="key">{{ key }}</li>
+      <li
+        class="item"
+        v-for="item of letters"
+        :key="item"
+        @touchstart="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
+        @click="handleLetterClick"
+        :ref="item"
+      >{{ item }}</li>
     </ul>
   </div>
 </template>
@@ -11,6 +20,56 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  updated () {
+    // this.$refs['A'][0].offsetTop is the height of "A" section to the top (header is NOT included)
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          // e.touches[0].clientY is the height of touch point to device top
+          // 79 is the height of the header
+          const touchY = e.touches[0].clientY - 79
+          // touchY - startY
+          const index = Math.floor((touchY - this.startY) / 20)
+          // console.log(index)
+          // console.log(startY)
+          if (index >= 0 && this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -13,19 +13,25 @@
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
           <div
-          class="button-wrapper"
-          v-for="item of hot"
-          :key="item.id">
+            class="button-wrapper"
+            v-for="item of hot"
+            :key="item.id"
+          >
             <div class="button">{{item.name}}</div>
           </div>
         </div>
       </div>
-      <div class="area" v-for="(item, key) of cities" :key="key">
+      <div
+        class="area"
+        v-for="(item, key) of cities"
+        :key="key"
+        :ref="key"
+      >
         <div class="title border-topbottom">{{ key }}</div>
         <div
-        class="item-list"
-        v-for="innerItem of item"
-        :key="innerItem.id"
+          class="item-list"
+          v-for="innerItem of item"
+          :key="innerItem.id"
         >
           <div class="item border-bottom">{{ innerItem.name }}</div>
         </div>
@@ -40,10 +46,20 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        // this method from better scroll
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
